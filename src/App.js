15ad4a17e69ba3155f5eb1af2c9e9801ece18091
kmui2 <kmui2@wisc.edu>
2017-09-29 01:22:08 -0500

    adding chartjs2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,185 @@
 import React, { Component } from 'react';
+import Dropzone from 'react-dropzone'
 import logo from './logo.svg';
 import './App.css';
+import Papa from 'papaparse';
+import {Bubble, Doughnut, Scatter} from 'react-chartjs-2';
+// import Plotly from 'plotly.js';
+const data = {
+  labels: ['January'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      borderColor: 'rgba(75,192,192,1)',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [{x:10,y:20,r:5}]
+    }
+  ]
+};
+
+const popData = {
+  datasets: [{
+    label: ['Deer Population'],
+    data: [{
+      x: 100,
+      y: 0,
+      r: 10
+    }, {
+      x: 60,
+      y: 30,
+      r: 20
+    }, {
+      x: 40,
+      y: 60,
+      r: 25
+    }, {
+      x: 80,
+      y: 80,
+      r: 50
+    }, {
+      x: 20,
+      y: 30,
+      r: 25
+    }, {
+      x: 0,
+      y: 100,
+      r: 5
+    }],
+    backgroundColor: "#FF9966"
+  }]
+};
+
+const dataD = {
+	labels: [
+		'Red',
+		'Green',
+		'Yellow'
+	],
+	datasets: [{
+		data: [300, 50, 100],
+		backgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		],
+		hoverBackgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		]
+	}]
+};
+
+const dataS = {
+  labels: ['Scatter'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      fill: false,
+      backgroundColor: 'rgba(75,192,192,0.4)',
+      pointBorderColor: 'rgba(75,192,192,1)',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [
+        { x: 65, y: 75 },
+        { x: 59, y: 49 },
+        { x: 80, y: 90 },
+        { x: 81, y: 29 },
+        { x: 56, y: 36 },
+        { x: 55, y: 25 },
+        { x: 40, y: 18 },
+      ]
+    }
+  ]
+};
 
 class App extends Component {
+  
+
+  constructor() {
+    super()
+    this.state = { files: [] }
+    
+    
+  }
+
+  onDrop(files) {
+    this.setState({
+      files
+    });
+    console.log(files);
+    Papa.parse(files[0], {
+      delimiter: ",",
+      header: true,
+      complete: function(results){
+        console.log(results);
+      },
+    });
+    Papa.parse(files[0], {
+      config: {
+          delimiter: ",",
+          header: true,
+          complete: function(results){
+            console.log(results);
+          },
+      },
+  });
+  }
+
+
   render() {
+    
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+          <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/4.3.6/papaparse.min.js"></script>
         </header>
+        <section>
+        <div className="dropzone">
+          <Dropzone onDrop={this.onDrop.bind(this)}>
+            <p>Try dropping some files here, or click to select files to upload.</p>
+          </Dropzone>
+        </div>
+        <aside>
+          <h2>Dropped files</h2>
+          <ul>
+            {
+              this.state.files.map(f => <li key={f.name}>{f.name} - {f.size} bytes</li>)
+            }
+          </ul>
+        </aside>
+      </section>
+      <div>
+        <h2>Bubble Example</h2>
+        <Scatter data={dataS} />
+      </div>
+      <div id="myDiv" style={{width: '480px', height: '400px'}}></div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
